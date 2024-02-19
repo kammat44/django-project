8e9b9ec1ae21a13d952e38b67cbcf2731627751f
Mateusz Kamiński <m.kaminski.gik@gmail.com>
2024-02-19 16:21:49 +0100

    initial commit

---
+++ challengebuddy/todolist_app/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect 
+from todolist_app.models import TaskList
+from todolist_app.forms import TaskForm
+from django.contrib import messages
+
+# Create your views here.
+def todolist(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        messages.success(request,("New Task Added!"))    
+        return redirect('todolist')    
+    else:
+        all_tasks =TaskList.objects.all 
+        return render(request, 'todolist.html', {'all_tasks': all_tasks})
+
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+    return redirect('todolist')
+
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance = task)
+        if form.is_valid():
+            form.save()
+        
+        messages.success(request,("Task Edited!"))    
+        return redirect('todolist')    
+    else:
+        task_obj = TaskList.objects.get(pk=task_id) 
+        return render(request, 'edit.html', {'task_obj': task_obj})
+
+def contact(request):
+    context = {
+        'contact_text':"Welcome To Contact Page."
+    }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        'about_text':"Welcome To About Page."
+    }
+    return render(request, 'about.html', context)
