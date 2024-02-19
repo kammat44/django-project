364c2013e946a49778765bcc149bf94d1596789e
Mateusz Kamiński <m.kaminski.gik@gmail.com>
2024-02-19 21:55:09 +0100

    pagination and index

--- challengebuddy/todolist_app/views.py
+++ challengebuddy/todolist_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 def todolist(request):
@@ -12,7 +13,11 @@ def todolist(request):
         messages.success(request,("New Task Added!"))    
         return redirect('todolist')    
     else:
-        all_tasks =TaskList.objects.all 
+        all_tasks =TaskList.objects.all() 
+        paginator = Paginator(all_tasks, 5)
+        page = request.GET.get('pg')
+        all_tasks = paginator.get_page(page)
+        
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 def delete_task(request, task_id):
@@ -32,6 +37,24 @@ def edit_task(request, task_id):
     else:
         task_obj = TaskList.objects.get(pk=task_id) 
         return render(request, 'edit.html', {'task_obj': task_obj})
+    
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+    return redirect('todolist')    
+
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    return redirect('todolist') 
+
+def index(request):
+    context = {
+        'index_text':"Welcome To Index Page"
+    }
+    return render(request, 'index.html', context)
 
 def contact(request):
     context = {
